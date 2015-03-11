b3e534ba8e3a41c29fca1908275babef9da3799a
Milan Zink <mzink@redhat.com>
2015-03-11 11:29:07 +0100

    Fix error.php - remove error detail

--- error.php
+++ error.php
@@ -12,7 +12,7 @@ $this->direction = $doc->direction;
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title><?php echo $this->error->getCode(); ?> - <?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></title>
+        <title><?php echo JText::_('JERROR_LAYOUT_PAGE_NOT_FOUND'); ?></title>
 
         <link rel="stylesheet" 
               href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
@@ -52,11 +52,6 @@ $this->direction = $doc->direction;
                 <a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a>
             </h1>
         </div><!-- end gotohomepage -->
-        <h3>
-            <?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?>
-        </h3>
-        <h2>#<?php echo $this->error->getCode(); ?>&nbsp;<?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?>
-        </h2>
-
+        
     </body>
 </html>
